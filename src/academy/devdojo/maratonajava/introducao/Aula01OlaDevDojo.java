5ecbeaebaa1435b0525e727b7025b7d05d670278
Vivian Barbosa Reis <srtvivian@hotmail.com>
2022-08-17 00:09:15 -0300

    Tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b></></>
+ */
+public class Aula01OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /*
+    Comentário de multiplas linhas
+     */
+    public static void main (String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, chat, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 127 ;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é: " + idade + " anos" );
+        System.out.println(falso);
+        System.out.println(caractere);
+    }
+
+}
